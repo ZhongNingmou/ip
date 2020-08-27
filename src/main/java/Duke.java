bdf90f42c608004164c4ca36d6ecfd2c13aa84ce
NAOMI\lemonhyuuga <lemonhyuuga@gmail.com>
2020-08-27 18:10:34 +0800

    Greet, echo, and exit when the user types bye.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,9 +8,18 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        String line = "_______________________";
-        System.out.println(line + "\nHello! I'm Duke\nWhat can I do for you?");
-        System.out.println(line + "\nBye. Hope to see you again soon!");
-        System.out.println(line);
+        String lineCutOff = "_______________________";
+        System.out.println(lineCutOff + "\nHello! I'm Duke\nWhat can I do for you?" + "\n" + lineCutOff);
+
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+        while(!(line.matches("Bye") ||line.matches("bye") ) ){
+            System.out.println(lineCutOff + "\n" + line + "\n" + lineCutOff);
+            line = in.nextLine();
+        }
+        System.out.println(lineCutOff + "\n" + line);
+        System.out.println(lineCutOff + "\nBye. Hope to see you again soon!" + "\n" + lineCutOff);
     }
 }
+
